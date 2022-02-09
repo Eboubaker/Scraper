8ddd28b54d91d3abb857026091cd70948519dfc7
Eboubaker Bekkouche <eboubakkar@gmail.com>
2022-02-09 21:30:30 +0100

    increased curl timeout to 1 minute.

--- src/Contracts/Scrapper.php
+++ src/Contracts/Scrapper.php
@@ -43,7 +43,7 @@ abstract class Scrapper
     {
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 60);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
         curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
         curl_setopt($ch, CURLOPT_HTTPHEADER, [

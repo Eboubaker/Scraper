be065e00ffef418c5c27d61c85e8e472f1476e1d
Eboubaker Bekkouche <eboubakkar@gmail.com>
2022-03-12 22:04:35 +0100

    refactor FacebookScrapper.php

--- src/Scrappers/FacebookScrapper.php
+++ src/Scrappers/FacebookScrapper.php
@@ -2,14 +2,20 @@
 
 namespace Eboubaker\Scrapper\Scrappers;
 
+use Eboubaker\Scrapper\App;
 use Eboubaker\Scrapper\Concerns\ScrapperUtils;
 use Eboubaker\Scrapper\Contracts\Scrapper;
+use Eboubaker\Scrapper\Exception\DownloadFailedException;
+use Eboubaker\Scrapper\Exception\ExpectationFailedException;
 use Eboubaker\Scrapper\Exception\WebPageNotLoadedException;
+use Eboubaker\Scrapper\Tools\Http\Document;
+use Eboubaker\Scrapper\Tools\Http\ThreadedDownloader;
+use Throwable;
 
 /**
  * @author Eboubaker Bekkouche <eboubakkar@gmail.com>
  */
-class FacebookScrapper implements Scrapper
+final class FacebookScrapper implements Scrapper
 {
     use ScrapperUtils;
 
@@ -26,33 +32,56 @@ class FacebookScrapper implements Scrapper
      */
     function download_media_from_html_document(Document $document): string
     {
-        if (preg_match("/\/login\/\?next=/", $final_url)) {
-            throw new WebPageNotLoadedException("Facebook redirected you to the login page, This post might be private, try logging in first");
+        if ($document->getContentLength() < bytes('300kb')) {
+            notice("Response size is too small: {}", style(human_readable_size($document->getContentLength()), 'red'));
         }
-        $data_bag = collect_all_json($html_document);
-        $image = data_get($data_bag, array_search_match($data_bag, [
-                "currMedia.image.uri",
-                "currMedia.__isMedia" => "/Photo/",
-            ]) . '.currMedia.image');
-        $route = data_get($data_bag, array_search_match($data_bag, [
-                "url",
-                "routePath",
-            ]) ?? '@@');
-        $videos = data_get($data_bag, array_search_match($data_bag, [
-                "data.video.story.attachments",
-            ]) . ".data.video.story.attachments");
-        if ($videos) {
-            foreach ($videos as $video) {
-                notice("VIDEO URL: {}", data_get($video, 'media.playable_url_quality_hd',
-                    fn() => data_get($video, 'media.playable_url')));
-            }
+        if (preg_match("/\/login\/\?next=/", $document->getFinalUrl())) {
+            throw new WebPageNotLoadedException("Facebook redirected me to the login page, This post might be private, try adding required header cookies");
         }
-        if (isset($image['uri'])) {
-            notice("IMAGE URL: {}", $image['uri']);
-        } else if (!$videos || count($videos) == 0) {
-            error("No media elements were found in this post");
+
+        $data_bag = $document->getDataBag();
+        $base = "https?:\/\/.{1,3}\.facebook\.com";
+        if (preg_match("/$base\/(?<poster>[^\/]+)\/videos\/(?<video_id>\d+)/", $document->getFinalUrl(), $matches)) {
+            download_by_video_id:
+            $owner = data_get($data_bag, array_search_match($data_bag, [
+                    "id" => "/$matches[video_id]/",
+                    "owner.name"
+                ]) . ".owner.name", fn() => data_get($matches, 'poster'));
+            info("Downloading Video $matches[video_id]" . putif($owner, " from $owner"));
+            $url = data_get($data_bag, array_search_match($data_bag, [
+                        "id" => "/$matches[video_id]/",
+                        "playable_url_quality_hd"
+                    ]) . ".playable_url_quality_hd")
+                ?? data_get($data_bag, array_search_match($data_bag, [
+                        "id" => "/$matches[video_id]/",
+                        "playable_url"
+                    ]) . ".playable_url");
+            $downloader = new ThreadedDownloader($url, 32);
+            $fname = normalize(App::cache_get('output_dir') . "/" . putif($owner, "$owner ") . $matches['video_id'] . ".mp4");
+            return $downloader->saveto($fname);
+        } else if (preg_match("/$base\/watch(\?.*?)v=(?<video_id>\d+)/", $document->getFinalUrl(), $matches)) {
+            goto download_by_video_id;
+        } else if (preg_match("/$base\/photo\/(\?.*?)fbid=(?<image_id>\d+)/", $document->getFinalUrl(), $matches)) {
+            download_by_image_id:
+            $owner = data_get($data_bag, array_search_match($data_bag, [
+                    "id" => "/$matches[image_id]/",
+                    "owner.name"
+                ]) . ".owner.name", fn() => data_get($matches, 'poster'));
+            info("Downloading Image $matches[image_id]" . putif($owner, " from $owner"));
+            $url = data_get($data_bag, array_search_match($data_bag, [
+                    "id" => "/$matches[image_id]/",
+                    "image.uri"
+                ]) . ".image.uri");
+            $downloader = new ThreadedDownloader($url, 8);
+            $fname = normalize(App::cache_get('output_dir') . "/" . putif($owner, "$owner ") . $matches['image_id'] . ".jpg");
+            return $downloader->saveto($fname);
+        } else if (preg_match("/$base\/(?<poster>[^\/]+)\/photos\/[^\/]+\/(?<image_id>\d+)/", $document->getFinalUrl(), $matches)) {
+            goto download_by_image_id;
+        } else if (preg_match("/$base\/(?<poster>[^\/]+)\/videos\/([^\/]+?\/)?(?<video_id>\d+)/", $document->getFinalUrl(), $matches)) {
+            goto download_by_video_id;
+        } else {
+            throw new ExpectationFailedException("No media elements were found in this post");
         }
-        return '';
     }
 }
 
